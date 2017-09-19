6c7f97c1054e8ed91185ef057e0e4e631ef61242
nichol alexander <nicholalexander@nca-deck.local>
2017-09-19 14:52:55 -0400

    default models, config models, cycle through models, lookup model keys.

--- lib/clarification/configuration.rb
+++ lib/clarification/configuration.rb
@@ -1,9 +1,10 @@
 module Clarification
   class Configuration
-    attr_accessor :api_key
+    attr_accessor :api_key, :default_models
 
     def initialize
       @api_key = 'YOUR_API_KEY'
+      @default_models = []
     end
 
   end

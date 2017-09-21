165c60741b408bbcb57ba401337af3ba527155d2
nichol alexander <nichol.alexander@gmail.com>
2017-09-21 14:44:05 -0400

    first pass at parsing non-concept responses.

--- lib/clarification/objectifier.rb
+++ lib/clarification/objectifier.rb
@@ -18,20 +18,34 @@ module Clarification
       if concepts
         begin
           concepts.each do |concept|
-            @concepts << OpenStruct.new(concept)
+            @concepts << OpenStruct.new(concept) # conecept = tag?
           end
         rescue
         end
       else
         data = @response_json.fetch("outputs").fetch(0).fetch("data", nil)
-        if !data.empty?
-          
-          # case @response[:focus].response_json['outputs'][0]['model']['name']
-          # when "focus"
-          # when "color"
-          # when "general_embedding"
-          # end
-          
+        unless data.empty?
+          case @response_json['outputs'][0]['model']['name']
+          when "focus"
+            # concept.class = concept::focus
+            focus = OpenStruct.new(data["focus"])
+            regions = []
+            data["regions"].each do |region|
+              regions << OpenStruct.new(region)
+            end
+            focus_hash = {focus: focus, regions: regions}
+            @concepts << OpenStruct.new(focus_hash)
+            @concepts.first.name = "focus"
+          when "color"
+            # concept.class = concept::color
+            data["colors"].each do |color|
+              @concepts << OpenStruct.new(color)
+            end
+          when "general_embedding"
+            # concept.class = concept::embedding
+            @concepts << OpenStruct.new(data["embeddings"][0])
+            @concepts.first.name = "embedding"
+          end
         end
       end
 

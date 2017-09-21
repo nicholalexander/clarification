bfd9e6d84a32793cca5c07f99bfbd9d8daa736f1
nichol alexander <nichol.alexander@gmail.com>
2017-09-21 14:56:33 -0400

    first pass at parsing non-concept responses. (#5)
    * first pass at parsing non-concept responses.
    
    * remove pry and unused rescue.

--- lib/clarification/objectifier.rb
+++ lib/clarification/objectifier.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 module Clarification
   class Objectifier
 
@@ -15,23 +13,34 @@ module Clarification
 
     def build_concept_objects
       concepts = @response_json.fetch("outputs").fetch(0).fetch("data", nil).fetch("concepts", nil)
-      if concepts
-        begin
-          concepts.each do |concept|
-            @concepts << OpenStruct.new(concept)
-          end
-        rescue
+      if concepts  
+        concepts.each do |concept|
+          @concepts << OpenStruct.new(concept) # conecept = tag?
         end
       else
         data = @response_json.fetch("outputs").fetch(0).fetch("data", nil)
-        if !data.empty?
-          
-          # case @response[:focus].response_json['outputs'][0]['model']['name']
-          # when "focus"
-          # when "color"
-          # when "general_embedding"
-          # end
-          
+        unless data.empty?
+          case @response_json['outputs'][0]['model']['name']
+          when "focus"
+            # concept.class = concept::focus
+            focus = OpenStruct.new(data["focus"])
+            regions = []
+            data["regions"].each do |region|
+              regions << OpenStruct.new(region)
+            end
+            focus_hash = {focus: focus, regions: regions}
+            @concepts << OpenStruct.new(focus_hash)
+            @concepts.first.name = "focus"
+          when "color"
+            # concept.class = concept::color
+            data["colors"].each do |color|
+              @concepts << OpenStruct.new(color)
+            end
+          when "general_embedding"
+            # concept.class = concept::embedding
+            @concepts << OpenStruct.new(data["embeddings"][0])
+            @concepts.first.name = "embedding"
+          end
         end
       end
 

da93b71c82d40cacccc4b03b208ac3afc05b2f61
nichol alexander <nichol.alexander@gmail.com>
2017-09-25 15:19:54 -0400

    nicer hash syntax for rubocop.

--- lib/clarification/requester/search_requester.rb
+++ lib/clarification/requester/search_requester.rb
@@ -49,7 +49,7 @@ module Clarification
                 data: {
                   concepts: [
                     {
-                      "name": "#{concept}"
+                      name: "#{concept}"
                     }
                   ]
                 }

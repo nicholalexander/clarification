52c44444fbdd041a420a026abcf0438a25504c8f
nichol alexander <nichol.alexander@gmail.com>
2017-09-28 12:36:59 -0400

    update readme.

--- lib/clarification/train.rb
+++ lib/clarification/train.rb
@@ -8,16 +8,19 @@ module Clarification
     def add_image(image, concept_array)
       training_requester = TrainRequester.new
       response = training_requester.add_image_with_concepts(image, concept_array)
+      return response # will be mostly status
     end
     
     def create_model(name, concept_array)
       training_requester = TrainRequester.new
       response = training_requester.create_model(name, concept_array)
+      return response # should contain id and set model id?
     end
     
     def train_model(id)
       training_requester = TrainRequester.new
       response = training_requester.train_model(id)
+      return response # should be status.
     end
 
   end

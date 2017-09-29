8a55ae99092a8dad48b9b36f0cdc6ce1a3c612ef
nichol alexander <nichol.alexander@gmail.com>
2017-09-29 15:24:04 -0400

    moved vcr spects to predict and use the udpated client.predict.by_url

--- spec/clarification/predict_spec.rb
+++ spec/clarification/predict_spec.rb
@@ -1,7 +1,39 @@
 require 'spec_helper'
 
-RSpec.describe Clarification::Train do
+RSpec.describe Clarification::Predict do
 
-  it "should be tested"
+    it "should return an array of concepts" do        
+      VCR.use_cassette('predict_cat') do
+        Clarification.configure do |config|
+          config.api_key = 'f7cc628178994e16b2470ae739ef927a'
+          config.default_public_models = [:food, :general]
+        end
+
+        images = YAML.load_file("spec/fixtures/images.yml")
+        url = images[:kitten]
+
+        client = Clarification::Client.new
+        response = client.predict.by_url(url)
+
+        expect(response[:general].concepts.class).to eq Array
+      end
+    end
+
+    it "should set @last_response" do
+      VCR.use_cassette('predict_cat', re_record_interval: 604800) do
+        Clarification.configure do |config|
+          config.api_key = 'f7cc628178994e16b2470ae739ef927a'
+          config.default_public_models = [:food, :general]
+        end
+
+        images = YAML.load_file("spec/fixtures/images.yml")
+        url = images[:kitten]
+
+        client = Clarification::Client.new
+        response = client.predict.by_url(url)
+
+        expect(client.predict.last_response).to eq response
+      end
+    end
 
 end

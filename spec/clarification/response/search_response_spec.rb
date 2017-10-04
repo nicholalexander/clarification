435343550c1a02b52be2e8d642709feee1d866a2
nichol alexander <nichol.alexander@gmail.com>
2017-10-04 13:00:19 -0400

    update guardfile to watch updates of lib files properly.

---
+++ spec/clarification/response/search_response_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+RSpec.describe Clarification::SearchResponse do
+  
+  it "should initialize with a raw response from the api and a concept object" do
+    
+    response = 1
+    
+  end
+
+
+end
---
+++ spec/clarification/search_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+RSpec.describe Clarification::Search do 
+
+  it "should be tested"
+  
+end

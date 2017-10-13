b1f4aacb1ef71b70bf4ca07cf19e51118fa01a70
nichol alexander <nichol.alexander@gmail.com>
2017-10-13 09:52:49 -0400

    revert.

--- lib/clarification/response/train_response.rb
+++ lib/clarification/response/train_response.rb
@@ -1,20 +1,27 @@
 module Clarification
-  # Process the response from the TrainRequest
+
   class TrainResponse < Response
+
     def initialize(raw_response)
       @raw_response = JSON.parse(raw_response)
     end
 
     def create_model
-      return nil if @raw_response['status']['code'] == 10_000
-      Model.new(id: @raw_response['model']['id'],
-                name: @raw_response['model']['name'])
+      if @raw_response['status']['code'] == 10000
+        Model.new(id: @raw_response['model']['id'],
+          name: @raw_response['model']['name'])
+      end
     end
 
     def train_model
-      return nil if @raw_response['status']['code'] == 10_000
-      Model.new(id: @raw_response['model']['id'],
-                name: @raw_response['model']['name'])
+      if @raw_response['status']['code'] == 10000
+        Model.new(id: @raw_response['model']['id'],
+          name: @raw_response['model']['name'])
+      end
     end
+
+
+
   end
 end
+

6d07302fb13fb7034f03870226d741239c65e993
nichol alexander <nichol.alexander@gmail.com>
2017-10-13 09:24:32 -0400

    refactor predict test.

--- spec/clarification/predict_spec.rb
+++ spec/clarification/predict_spec.rb
@@ -2,18 +2,21 @@ require 'spec_helper'
 
 RSpec.describe Clarification::Predict do
 
+    before do
+      Clarification.configure do |config|
+        config.api_key = 'f7cc628178994e16b2470ae739ef927a'
+        config.default_public_models = [:food, :general]
+      end
+
+      images = YAML.load_file("spec/fixtures/images.yml")
+      @url = images[:kitten]
+    end
+
     it "should return an array of concepts" do        
       VCR.use_cassette('predict_cat') do
-        Clarification.configure do |config|
-          config.api_key = 'f7cc628178994e16b2470ae739ef927a'
-          config.default_public_models = [:food, :general]
-        end
-
-        images = YAML.load_file("spec/fixtures/images.yml")
-        url = images[:kitten]
 
         client = Clarification::Client.new
-        response = client.predict.by_url(url)
+        response = client.predict.by_url(@url)
 
         expect(response[:general].concepts.class).to eq Array
       end
@@ -21,16 +24,9 @@ RSpec.describe Clarification::Predict do
 
     it "should set @last_response" do
       VCR.use_cassette('predict_cat', re_record_interval: 604800) do
-        Clarification.configure do |config|
-          config.api_key = 'f7cc628178994e16b2470ae739ef927a'
-          config.default_public_models = [:food, :general]
-        end
-
-        images = YAML.load_file("spec/fixtures/images.yml")
-        url = images[:kitten]
 
         client = Clarification::Client.new
-        response = client.predict.by_url(url)
+        response = client.predict.by_url(@url)
 
         expect(client.predict.last_response).to eq response
       end

d63be201ff42d68432ee3e63535cb3664b22c1ed
nichol alexander <nichol.alexander@gmail.com>
2017-10-13 09:49:41 -0400

    rubocop compliance

--- lib/clarification/response/train_response.rb
+++ lib/clarification/response/train_response.rb
@@ -1,27 +1,20 @@
 module Clarification
-
+  # Process the response from the TrainRequest
   class TrainResponse < Response
-
     def initialize(raw_response)
       @raw_response = JSON.parse(raw_response)
     end
 
     def create_model
-      if @raw_response['status']['code'] == 10000
-        Model.new(id: @raw_response['model']['id'],
-          name: @raw_response['model']['name'])
-      end
+      return nil if @raw_response['status']['code'] == 10_000
+      Model.new(id: @raw_response['model']['id'],
+                name: @raw_response['model']['name'])
     end
 
     def train_model
-      if @raw_response['status']['code'] == 10000
-        Model.new(id: @raw_response['model']['id'],
-          name: @raw_response['model']['name'])
-      end
+      return nil if @raw_response['status']['code'] == 10_000
+      Model.new(id: @raw_response['model']['id'],
+                name: @raw_response['model']['name'])
     end
-
-
-
   end
 end
-
